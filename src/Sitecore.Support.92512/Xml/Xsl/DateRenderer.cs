be866c148bacdc3d40872b47c25109983d9364e3
Kirill Volovitskiy <kiv@sitecore.net>
2017-10-20 11:16:30 +0300

    Original GetCultureInfo method of  the Sitecore.Xml.Xsl.DateRenderer

---
+++ src/Sitecore.Support.92512/Xml/Xsl/DateRenderer.cs
@@ -0,0 +1,24 @@
+﻿namespace Sitecore.Support.Xml.Xsl
+{
+    using Sitecore.Diagnostics;
+    public class DateRenderer : Sitecore.Xml.Xsl.DateRenderer
+    {
+        protected override System.Globalization.CultureInfo GetCultureInfo()
+        {
+            System.Globalization.CultureInfo cultureInfo = Context.Language.CultureInfo;
+            string text = Parameters["culture"];
+            if (!string.IsNullOrEmpty(text))
+            {
+                try
+                {
+                    cultureInfo = System.Globalization.CultureInfo.GetCultureInfo(text);
+                }
+                catch (System.Exception exception)
+                {
+                    Log.Warn(string.Format("Cannot parse cuture info '{0}'.", text), exception, this);
+                }
+            }
+            return cultureInfo;
+        }
+    }
+}
