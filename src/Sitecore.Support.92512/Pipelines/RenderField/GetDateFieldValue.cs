feb2883ee0f8573ea6106fb3afd21403133405b4
Kirill Volovitskiy <kiv@sitecore.net>
2017-10-20 11:38:33 +0300

    Fix bug

---
+++ src/Sitecore.Support.92512/Pipelines/RenderField/GetDateFieldValue.cs
@@ -0,0 +1,10 @@
+﻿namespace Sitecore.Support.Pipelines.RenderField
+{
+    public class GetDateFieldValue : Sitecore.Pipelines.RenderField.GetDateFieldValue
+    {
+        protected override Sitecore.Xml.Xsl.DateRenderer CreateRenderer()
+        {
+            return new Sitecore.Support.Xml.Xsl.DateRenderer();
+        }
+    }
+}
--- src/Sitecore.Support.92512/Xml/Xsl/DateRenderer.cs
+++ src/Sitecore.Support.92512/Xml/Xsl/DateRenderer.cs
@@ -1,11 +1,49 @@
 ﻿namespace Sitecore.Support.Xml.Xsl
 {
+    using Sitecore.Data;
+    using Sitecore.Data.Fields;
+    using Sitecore.Data.Items;
+    using Sitecore.Data.Managers;
     using Sitecore.Diagnostics;
     public class DateRenderer : Sitecore.Xml.Xsl.DateRenderer
     {
         protected override System.Globalization.CultureInfo GetCultureInfo()
         {
             System.Globalization.CultureInfo cultureInfo = Context.Language.CultureInfo;
+
+            if (cultureInfo != null && cultureInfo.IsNeutralCulture)
+            {
+                try
+                {
+                    ID languageItemId = LanguageManager.GetLanguageItemId(Sitecore.Context.Language, Sitecore.Context.Database);
+                    if (!languageItemId.IsNull)
+                    {
+                        Item languageItem = Sitecore.Context.Database.GetItem(languageItemId, Sitecore.Context.Language, Sitecore.Data.Version.Latest);
+
+                        if (languageItem != null)
+                        {
+                            Field regionalISOCode = languageItem.Fields["{0620F810-9294-4F14-AF9F-F5772EFCA0B2}"];
+
+                            if (regionalISOCode != null && !string.IsNullOrEmpty(regionalISOCode.Value))
+                            {
+                                try
+                                {
+                                    cultureInfo = System.Globalization.CultureInfo.GetCultureInfo(regionalISOCode.Value);
+                                }
+                                catch (System.Exception exception)
+                                {
+                                    Log.Warn(string.Format("Sitecore.Support.92512: Cannot parse cuture info from RegionalISOCode field '{0}'.", regionalISOCode.Value), exception, this);
+                                }
+                            }
+                        }
+                    }
+                }
+                catch (System.Exception exception)
+                {
+                    Log.Warn("Sitecore.Support.92512: Cannot retrieve data from RegionalISOCode field.", exception, this);
+                }
+            }
+
             string text = Parameters["culture"];
             if (!string.IsNullOrEmpty(text))
             {
